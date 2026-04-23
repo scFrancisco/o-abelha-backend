666fbbb54afceab678fcc00329f5759cbee91b18
SCFrancisco2221857 <scfranciscojr2004@gmail.com>
2026-04-23 09:58:31 +0100

    sd

--- src/main/java/com/example/oabelha/CorsConfig.java
+++ src/main/java/com/example/oabelha/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.oabelha;
+package com.example.oabelha.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,8 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
